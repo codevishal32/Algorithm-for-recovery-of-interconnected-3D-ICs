bea85dd01ec9031cd0adff1d5000765e12607e9f
Vishal <82036957+codevishal32@users.noreply.github.com>
2024-06-30 15:21:58 +0530

    Update code

--- Grid_based_Regular_Grouping.cpp
+++ Grid_based_Regular_Grouping.cpp
@@ -13,8 +13,9 @@ const int die_height = 50;
 pair<vector<pair<double, double>>, vector<pair<double, double>>> generateTestCase()
 {
     // Generate values for n and m
-    int n = rand() % 60 + 11; // n is greater than 10
-    int m = rand() % 25 + 4;  // m is greater than 3
+    srand(static_cast<unsigned int>(time(0)));
+    int n = 62; // n is greater than 10
+    int m = 17;  // m is greater than 3
 
     // Ensure the ratio between functional TSVs and redundant TSVs is greater than 2
     while (static_cast<double>(n) / m <= 2)
@@ -22,7 +23,7 @@ pair<vector<pair<double, double>>, vector<pair<double, double>>> generateTestCas
         n = rand() % 60 + 11;
         m = rand() % 25 + 4;
     }
-
+   
     // Generate functional TSV coordinates
     vector<pair<double, double>> functional_tsvs;
     for (int i = 0; i < n; ++i)
@@ -68,19 +69,16 @@ std::set<int> generateRandomNumbers(int n, int N, int delay)
     return uniqueRandomNumbers;
 }
 
-double distance(pair<double, double> a, pair<double, double> b)
-{
-    return sqrt((a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second));
-}
 
+double distance(const std::pair<double, double>& a, const std::pair<double, double>& b)
+{
+    return std::sqrt(std::pow(a.first - b.first, 2) + std::pow(a.second - b.second, 2));
+}
 int main()
 {
-    int t;
-    cin >> t;
-    for (int i = 0; i < t; i++)
-    {
+    
         pair<vector<pair<double, double>>, vector<pair<double, double>>> coordinate = generateTestCase();
-        int die_side, Nfunc, Nred, Nmux;
+        int die_side=50, Nfunc, Nred, Nmux;
         Nfunc = coordinate.first.size();
         Nred = coordinate.second.size();
         cout << "Number of functional tsvs : " << Nfunc << '\n';
@@ -99,49 +97,41 @@ int main()
             cout << itr.first << " " << itr.second << "\n";
             rdtTsv.push_back({(double)itr.first, (double)itr.second});
         }
-        // std::cout << "Enter the values of die area and the number of functional and redundant TSVs\n";
-        // cin >> die_side >> Nfunc >> Nred;
-        // std::cout << "Enter the co-ordinates of functinal TSVs on the die\n";
-        // vector<pair<double, double>> fncTsv, rdtTsv;
-        // for (int i = 0; i < Nfunc; i++)
-        // {
-        //     double x, y;
-        //     cin >> x >> y;
-        //     fncTsv.push_back({x, y});
-        // }
-        // std::cout << "Enter the co-ordinates of redundant TSVs on the die\n";
-        // for (int i = 0; i < Nred; i++)
-        // {
-        //     double x, y;
-        //     cin >> x >> y;
-        //     rdtTsv.push_back({x, y});
-        // }
-
-        double redious = die_side / Nred;
+        
+        double redious = (double)die_side / Nred;
 
         int max_grp_size = ceil((double)Nfunc / (double)Nred);
         vector<pair<double, double>> groups[Nred];
 
         vector<bool> done(Nfunc, false);
         int ind = 0, countfnc = 0;
+
+        vector<vector<pair<double, double>>> parent(Nred, vector<pair<double, double>>(Nfunc));
+
+
         for (int i = 0; i < Nred; i++)
         {
             queue<pair<double, double>> qu;
             qu.push(rdtTsv[i]);
+            
             map<pair<double, double>, bool> visited;
             visited[rdtTsv[i]] = true;
+            double var_rad=redious;
             while (groups[i].size() < max_grp_size)
             {
 
                 pair<double, double> current = qu.front();
                 qu.pop();
                 for (int j = 0; j < Nfunc; j++)
-                {
-                    if (!visited[fncTsv[j]] && distance(current, fncTsv[j]) <= redious)
-                    {
+                {   
+
+                    
+                    if (!visited[fncTsv[j]] && distance(current, fncTsv[j]) <= var_rad)
+                    {  
                         groups[i].push_back(fncTsv[j]);
                         qu.push(fncTsv[j]);
                         visited[fncTsv[j]] = true;
+                        parent[i][j] = current;
                         if (!done[j])
                             countfnc++;
 
@@ -151,11 +141,18 @@ int main()
                     {
                         break;
                     }
+
+                }
+                if(groups[i].size() < max_grp_size && qu.empty()){
+                    qu.push(rdtTsv[i]);
+                    var_rad += 1;
                 }
             }
         }
+       
+        cout<<"total functional tsv done "<< countfnc <<'\n';
       
-            for (int j = 0; j < Nfunc; j++)
+        for (int j = 0; j < Nfunc; j++)
             {
                 if (!done[j])
                 {
@@ -182,6 +179,7 @@ int main()
                         groups[idx1].push_back(fncTsv[j]);
                         countfnc++;
                         done[j] = true;
+                        parent[idx1][j] = rdtTsv[idx1];
                     }
                     else
                     {
@@ -192,6 +190,7 @@ int main()
                             {
                                 groups[i].push_back(fncTsv[j]);
                                 done[j] = true;
+                                parent[i][j] = fncTsv[idx2];
                                 break;
                             }
                         }
@@ -209,11 +208,42 @@ int main()
             }
             cout << '\n';
         }
+        
+        cout<<'\n';
 
+        for (int i = 0; i < Nred; i++)
+        {  
+           for (int j = i+1; j < Nred; j++)
+            { 
+              vector<pair<double, double>> save;
+               for(int k=0; k<groups[i].size(); k++){
+                  for(int l=0; l<groups[j].size(); l++){
+                       if(groups[i][k] == groups[j][l]){
+                        save.push_back(groups[i][k]);
+                       }
+                  }
+             }
+            
+           if(save.size()>0){
+             cout << "Shared functional TSVs between groups " << i+1 << " and " << j+1 << '\n';
+            for(auto it:save){
+                cout << '(' << it.first << ',' << it.second << ')' << ' ';
+            }
+             cout<<'\n';
+           }
+         }
+        
+        }
+
+       cout<<'\n';
+
+        
         for (int faulty = 1; faulty <= Nred; faulty++)
         {
             int delay = 200;
             int cnt = 0;
+            int num_of_mux=0, avg_num_of_mux=0;
+            double total_wire_length=0, avg_wire_length=0;
             for (int round = 0; round < 100; round++)
             {
                 set<int> faultyOnes = generateRandomNumbers(faulty, Nfunc, delay);
@@ -223,29 +253,60 @@ int main()
                 {
 
                     bool fixed = false;
-
+                    int cnt_of_rdt=0;
                     for (int i = 0; i < Nred; i++)
                     {
                         auto it = find(groups[i].begin(), groups[i].end(), fncTsv[idx]);
-                        if (it != groups[i].end() && !usedred[i])
-                        {
+
+                        if(it != groups[i].end()){
+                            cnt_of_rdt++; 
+                        }
+
+                        if (it != groups[i].end() && !usedred[i] && !fixed)
+                        {   
                             usedred[i] = true;
                             fixed = true;
-                            break;
+
+                            double wire_length=0;
+                            int ind=idx;
+                            while(true){
+                                wire_length = max(wire_length, distance(fncTsv[ind], parent[i][ind]));
+                                bool hmm=false;
+                                for (int j = 0; j < Nfunc; j++){
+                                    if(fncTsv[j]==parent[i][ind]){
+                                         ind = j;
+                                         hmm=true;
+                                         break;
+                                    }
+                                }
+                                if(!hmm){
+                                    break;
+                                }
+                            }
+                            total_wire_length += wire_length;
+                           
                         }
                     }
                     if (fixed)
                         totFixed++;
+                    num_of_mux += ceil(log2(cnt_of_rdt));
                 }
 
-                if (totFixed == faulty)
+                if (totFixed == faulty){
                     cnt++;
+                }
+                avg_wire_length += total_wire_length;
+                total_wire_length=0;
+                avg_num_of_mux += num_of_mux;
+                num_of_mux = 0;
+
             }
             delay -= 33;
             cout << "recovery rate for N_functional = " << Nfunc << ", N_redundent = " << Nred << " and N_faulty = " << faulty << " is : " << cnt << " percent\n";
+            cout << "Averege minimum wire length required = " << avg_wire_length/100 << " unit" << '\n';
+            cout << "Averege number of mux required = " << ceil(avg_num_of_mux/100) << '\n';
         }
         cout << "----------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
-    }
-
+    
     return 0;
 }
